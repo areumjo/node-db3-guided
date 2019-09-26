872260dad876ea7e320b27f3c5d6c5fbaafecba5
Areum Jo <areumjo1@gmail.com>
2019-09-26 14:34:50 -0700

    Added JOIN query and modular method for find()

--- users/user-router.js
+++ users/user-router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
+const Users = require('./user-model.js');
 const db = require('../data/db-config.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  db('users')
+  Users.find()
   .then(users => {
     res.json(users);
   })
@@ -76,5 +77,33 @@ router.delete('/:id', (req, res) => {
     res.status(500).json({ message: 'Failed to delete user' });
   });
 });
+
+// all posts by user id
+router.get('/:id/posts', (req, res) => {
+  const { id } = req.params;
+
+  // // SELECT * FROM posts WHERE user_id = id; => will give us just post with user_id but we want to know username then JOIN
+  // db('posts').where({ user_id: id })
+  // .then(posts => {
+  //   res.json(posts);
+  // })
+  // .catch(err => {
+  //   res.status(500).json({ message: "failed to get posts"})
+  // });
+
+  // SELECT posts.id, contents, username FROM posts 
+  // JOIN users ON posts.user_id = users.user_id;
+  db('posts as p')
+    .join('users as u', 'u.id', 'p.user_id')
+    .select('p.id', 'p.contents', 'u.username')
+    .where({ user_id: id })
+  .then(posts => {
+    res.json(posts);
+  })
+  .catch(err => {
+    res.status(500).json({ message: "failed to get posts"})
+  }); 
+
+})
 
 module.exports = router;
